e18a0c995a485e8b35a9b62c415f7ac2467a6a18
N4N1T0 <68034867+N4N1T0@users.noreply.github.com>
2023-11-27 02:32:13 +0100

    feat: add astro integrations and modified SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import sitemap from "@astrojs/sitemap";
+import robotsTxt from 'astro-robots-txt';
+import vercel from "@astrojs/vercel/serverless";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: process.env.CI ? 'https://www.nanofighters.club' : 'http://localhost:4321',
-  integrations: [tailwind(), sitemap()]
+  integrations: [tailwind(), sitemap(), robotsTxt(), partytown()],
+  output: "server",
+  adapter: vercel()
 });
