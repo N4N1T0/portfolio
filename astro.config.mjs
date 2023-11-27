3c1b62ccfd84a4cfb78a7b3eb2079b9a9f6ff457
N4N1T0 <68034867+N4N1T0@users.noreply.github.com>
2023-11-27 02:56:07 +0100

    refactor: modified meta refresh and astr.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,20 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import robotsTxt from 'astro-robots-txt';
 import vercel from "@astrojs/vercel/serverless";
-
 import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  site: process.env.CI ? 'https://portfolio-nine-theta-20.vercel.app' : 'http://localhost:4321',
-  integrations: [tailwind(), sitemap(), robotsTxt(), partytown()],
+  site: process.env.CI ? 'https://www.adrian-alvarez.dev' : 'http://localhost:4321',
+  integrations: [tailwind(), sitemap(), robotsTxt({
+    sitemap: 'https://www.adrian-alvarez.dev/sitemap-0.xml',
+    host: 'adrian-alvarez.dev'
+  }), partytown({
+    config: {
+      forward: ["dataLayer.push"],
+      debug: false
+    }
+  })],
   output: "server",
   adapter: vercel()
 });
