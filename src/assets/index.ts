00d4701b568b5f73f093d63b26a11f420e269237
N4N1T0 <68034867+N4N1T0@users.noreply.github.com>
2024-07-24 17:12:47 +0200

    feat: Delete all projects and New ones

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,21 +1,25 @@
-import ToughShowcase from './tough-showcase.webp'
-import ToughStyle from './tough-style.webp'
-import StrikingGoldShowcase from './striking-gold-showcase.webp'
-import StrikingGoldStyle from './striking-gold-style.webp'
-import NFCShowcase from './nano-fighters-club-showcase.webp'
-import NFCStyle from './nano-fighters-club-style.webp'
-import FinanceCloverShowcase from './finace-clover-showcase.webp'
-import FinanceCloverView from './finace-clover-view.mp4'
-import TTStyle from './three-shirt-style.webp'
+import ToughShowcase from "./tough-showcase.webp";
+import ToughStyle from "./tough-style.webp";
+import StrikingGoldShowcase from "./striking-gold-showcase.webp";
+import StrikingGoldStyle from "./striking-gold-style.webp";
+import NFCShowcase from "./nano-fighters-club-showcase.webp";
+import NFCStyle from "./nano-fighters-club-style.webp";
+import FinanceCloverShowcase from "./finace-clover-showcase.webp";
+import FinanceCloverView from "./finace-clover-view.mp4";
+import TTStyle from "./three-shirt-style.webp";
+import BSShowcase from "./backswing-showcase.webp";
+import BSStyle from "./backswing-style.webp";
 
 export {
-  ToughShowcase,
-  ToughStyle,
-  StrikingGoldStyle,
-  StrikingGoldShowcase,
-  NFCShowcase,
-  NFCStyle,
-  FinanceCloverShowcase,
-  FinanceCloverView,
-  TTStyle
-}
+	ToughShowcase,
+	ToughStyle,
+	StrikingGoldStyle,
+	StrikingGoldShowcase,
+	NFCShowcase,
+	NFCStyle,
+	FinanceCloverShowcase,
+	FinanceCloverView,
+	TTStyle,
+	BSShowcase,
+	BSStyle,
+};
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,177 +1,162 @@
 export const services = [
-  {
-    label: 'Web Development',
-    info: 'Elevate your online presence! With my expertise in web development, I leverage cutting-edge technology and a performance-driven approach to deliver digital experiences that are truly exceptional.'
-  },
-  {
-    label: 'App Development',
-    info: 'Embark on a digital journey! Utilizing my skills in app development, I design and develop feature-rich, scalable solutions using the latest technologies. Get ready for an immersive user experience like never before.'
-  },
-  {
-    label: 'SEO Audit',
-    info: 'Boost your online visibility! My SEO audit offers a comprehensive analysis and optimization strategy, ensuring your website takes center stage in search rankings.'
-  },
-  {
-    label: 'Email Marketing',
-    info: 'Stand out in the inbox! With my expertise in email marketing, I create captivating campaigns that drive traffic and sales, leveraging the latest technologies.'
-  },
-  {
-    label: 'Social Media Marketing',
-    info: 'Grow your brand&apos;s presence! Through engaging content tailored to your audience, my social media marketing services help you connect with customers and expand your reach.'
-  },
-  {
-    label: 'CRO',
-    info: 'Optimize customer retention! With my expertise in Conversion Rate Optimization (CRO), I develop customized solutions to enhance ecommerce growth by maximizing customer engagement and retention.'
-  }
+	{
+		label: "Web Development",
+		info: "Elevate your online presence! With my expertise in web development, I leverage cutting-edge technology and a performance-driven approach to deliver digital experiences that are truly exceptional.",
+	},
+	{
+		label: "App Development",
+		info: "Embark on a digital journey! Utilizing my skills in app development, I design and develop feature-rich, scalable solutions using the latest technologies. Get ready for an immersive user experience like never before.",
+	},
+	{
+		label: "SEO Audit",
+		info: "Boost your online visibility! My SEO audit offers a comprehensive analysis and optimization strategy, ensuring your website takes center stage in search rankings.",
+	},
+	{
+		label: "Email Marketing",
+		info: "Stand out in the inbox! With my expertise in email marketing, I create captivating campaigns that drive traffic and sales, leveraging the latest technologies.",
+	},
+	{
+		label: "Social Media Marketing",
+		info: "Grow your brand&apos;s presence! Through engaging content tailored to your audience, my social media marketing services help you connect with customers and expand your reach.",
+	},
+	{
+		label: "CRO",
+		info: "Optimize customer retention! With my expertise in Conversion Rate Optimization (CRO), I develop customized solutions to enhance ecommerce growth by maximizing customer engagement and retention.",
+	},
 ];
-
 
 export const servicesEspañol = [
-  {
-    etiqueta: 'Desarrollo Web',
-    info: '¡Eleva tu presencia en línea! Con mi experiencia en desarrollo web, aprovecho la tecnología de vanguardia y un enfoque orientado al rendimiento para ofrecer experiencias digitales verdaderamente excepcionales.'
-  },
-  {
-    etiqueta: 'Desarrollo de Apps',
-    info: '¡Embárcate en un viaje digital! Utilizando mis habilidades en desarrollo de aplicaciones, diseño y desarrollo soluciones ricas en funciones y escalables utilizando las últimas tecnologías. Prepárate para una experiencia de usuario inmersiva como nunca antes.'
-  },
-  {
-    etiqueta: 'Auditoría SEO',
-    info: '¡Mejora tu visibilidad en línea! Mi auditoría SEO ofrece un análisis exhaustivo y una estrategia de optimización, asegurando que tu sitio web tome el centro del escenario en los rankings de búsqueda.'
-  },
-  {
-    etiqueta: 'Email Marketing',
-    info: '¡Destácate en la bandeja de entrada! Con mi experiencia en marketing por correo electrónico, creo campañas cautivadoras que impulsan el tráfico y las ventas, aprovechando las últimas tecnologías.'
-  },
-  {
-    etiqueta: 'Marketing en Redes',
-    info: '¡Haz crecer la presencia de tu marca! A través de contenido atractivo adaptado a tu audiencia, mis servicios de marketing en redes sociales te ayudan a conectar con los clientes y expandir tu alcance.'
-  },
-  {
-    etiqueta: 'CRO',
-    info: '¡Optimiza la retención de clientes! Con mi experiencia en Optimización de la Tasa de Conversión (CRO), desarrollo soluciones personalizadas para mejorar el crecimiento del comercio electrónico al maximizar el compromiso y la retención de los clientes.'
-  }
+	{
+		etiqueta: "Desarrollo Web",
+		info: "¡Eleva tu presencia en línea! Con mi experiencia en desarrollo web, aprovecho la tecnología de vanguardia y un enfoque orientado al rendimiento para ofrecer experiencias digitales verdaderamente excepcionales.",
+	},
+	{
+		etiqueta: "Desarrollo de Apps",
+		info: "¡Embárcate en un viaje digital! Utilizando mis habilidades en desarrollo de aplicaciones, diseño y desarrollo soluciones ricas en funciones y escalables utilizando las últimas tecnologías. Prepárate para una experiencia de usuario inmersiva como nunca antes.",
+	},
+	{
+		etiqueta: "Auditoría SEO",
+		info: "¡Mejora tu visibilidad en línea! Mi auditoría SEO ofrece un análisis exhaustivo y una estrategia de optimización, asegurando que tu sitio web tome el centro del escenario en los rankings de búsqueda.",
+	},
+	{
+		etiqueta: "Email Marketing",
+		info: "¡Destácate en la bandeja de entrada! Con mi experiencia en marketing por correo electrónico, creo campañas cautivadoras que impulsan el tráfico y las ventas, aprovechando las últimas tecnologías.",
+	},
+	{
+		etiqueta: "Marketing en Redes",
+		info: "¡Haz crecer la presencia de tu marca! A través de contenido atractivo adaptado a tu audiencia, mis servicios de marketing en redes sociales te ayudan a conectar con los clientes y expandir tu alcance.",
+	},
+	{
+		etiqueta: "CRO",
+		info: "¡Optimiza la retención de clientes! Con mi experiencia en Optimización de la Tasa de Conversión (CRO), desarrollo soluciones personalizadas para mejorar el crecimiento del comercio electrónico al maximizar el compromiso y la retención de los clientes.",
+	},
 ];
-
 
 export const works = [
-  {
-    name: 'tough',
-    info: 'Ecommerce',
-    new: true,
-    inProcess: false
-  },
-  {
-    name: 'striking gold',
-    info: 'Ai Powered Blog',
-    new: false,
-    inProcess: false
-  },
-  {
-    name: 'nano fighters club',
-    info: 'Astro SEO Enhanced',
-    new: false,
-    inProcess: false
-  },
-  {
-    name: 'finance clover',
-    info: 'Data Analysis Powered Dashboard',
-    new: false,
-    inProcess: false
-  },
-  {
-    name: 'three tshirt',
-    info: '3D Graphics App',
-    new: false,
-    inProcess: false
-  },
-  {
-    name: 'portfolio',
-    info: 'Astro Animated Site',
-    new: false,
-    inProcess: true
-  },
-  {
-    name: 'masobi',
-    info: 'Cutting Edge Site',
-    new: false,
-    inProcess: true
-  }
-]
-
-export const worksEspañol = [
-  {
-    nombre: 'tough',
-    informacion: 'Ecommerce',
-    nuevo: true,
-    enProceso: false
-  },
-  {
-    nombre: 'striking gold',
-    informacion: 'Blog Potenciado por Inteligencia Artificial',
-    nuevo: false,
-    enProceso: false
-  },
-  {
-    nombre: 'nano fighters club',
-    informacion: 'Astro SEO Mejorado',
-    nuevo: false,
-    enProceso: false
-  },
-  {
-    nombre: 'finance clover',
-    informacion: 'Panel Potenciado por Análisis de Datos',
-    nuevo: false,
-    enProceso: false
-  },
-  {
-    nombre: 'three tshirt',
-    informacion: 'Aplicación de Gráficos 3D',
-    nuevo: false,
-    enProceso: false
-  },
-  {
-    nombre: 'portfolio',
-    informacion: 'Sitio Astro Animado',
-    nuevo: false,
-    enProceso: true
-  },
-  {
-    nombre: 'masobi',
-    informacion: 'Sitio de Next.js 14',
-    nuevo: false,
-    enProceso: true
-  }
+	{
+		name: "Lavanda del Lago",
+		info: "Sanity Ecommerce",
+		new: true,
+		inProcess: true,
+	},
+	{
+		name: "backswing",
+		info: "Woocommerce Headless",
+		new: true,
+		inProcess: false,
+	},
+	{
+		name: "tough",
+		info: "Next.js Ecommerce",
+		new: false,
+		inProcess: false,
+	},
+	{
+		name: "striking gold",
+		info: "Ai Powered Blog",
+		new: false,
+		inProcess: false,
+	},
+	{
+		name: "nano fighters club",
+		info: "Astro SEO Enhanced",
+		new: false,
+		inProcess: false,
+	},
 ];
 
+export const worksEspañol = [
+	{
+		nombre: "tough",
+		informacion: "Ecommerce",
+		nuevo: true,
+		enProceso: false,
+	},
+	{
+		nombre: "striking gold",
+		informacion: "Blog Potenciado por Inteligencia Artificial",
+		nuevo: false,
+		enProceso: false,
+	},
+	{
+		nombre: "nano fighters club",
+		informacion: "Astro SEO Mejorado",
+		nuevo: false,
+		enProceso: false,
+	},
+	{
+		nombre: "finance clover",
+		informacion: "Panel Potenciado por Análisis de Datos",
+		nuevo: false,
+		enProceso: false,
+	},
+	{
+		nombre: "three tshirt",
+		informacion: "Aplicación de Gráficos 3D",
+		nuevo: false,
+		enProceso: false,
+	},
+	{
+		nombre: "portfolio",
+		informacion: "Sitio Astro Animado",
+		nuevo: false,
+		enProceso: true,
+	},
+	{
+		nombre: "masobi",
+		informacion: "Sitio de Next.js 14",
+		nuevo: false,
+		enProceso: true,
+	},
+];
 
 export const diplomas = [
-  {
-    label: 'FCC Responsive Web Design',
-    link: 'https://www.freecodecamp.org/certification/fcc0c7054f7-09c1-43bf-8f90-c9121bc86e5a/responsive-web-design'
-  },
-  {
-    label: 'FCC JavaScript Algorithms and Data Structure',
-    link: 'https://www.freecodecamp.org/certification/fcc0c7054f7-09c1-43bf-8f90-c9121bc86e5a/javascript-algorithms-and-data-structures'
-  },
-  {
-    label: 'FCC Front End Development Libraries',
-    link: 'https://www.freecodecamp.org/certification/fcc0c7054f7-09c1-43bf-8f90-c9121bc86e5a/front-end-development-libraries'
-  },
-  {
-    label: 'FCC Data Visualization',
-    link: 'https://www.freecodecamp.org/certification/fcc0c7054f7-09c1-43bf-8f90-c9121bc86e5a/data-visualization'
-  },
-  {
-    label: 'FCC Back End Development and APIs',
-    link: 'https://www.freecodecamp.org/certification/fcc0c7054f7-09c1-43bf-8f90-c9121bc86e5a/back-end-development-and-apis'
-  },
-  {
-    label: 'Apollo Graph Developer - Associate',
-    link: 'https://www.apollographql.com/tutorials/certifications/2877a312-31ad-44a0-bd0c-febb6dc9a4ca'
-  },
-  {
-    label: 'Google SkillsShop Diplomas',
-    link: 'https://skillshop.exceedlms.com/profiles/e041a50daf52423ca72f738e181e05ed'
-  }
-]
+	{
+		label: "FCC Responsive Web Design",
+		link: "https://www.freecodecamp.org/certification/fcc0c7054f7-09c1-43bf-8f90-c9121bc86e5a/responsive-web-design",
+	},
+	{
+		label: "FCC JavaScript Algorithms and Data Structure",
+		link: "https://www.freecodecamp.org/certification/fcc0c7054f7-09c1-43bf-8f90-c9121bc86e5a/javascript-algorithms-and-data-structures",
+	},
+	{
+		label: "FCC Front End Development Libraries",
+		link: "https://www.freecodecamp.org/certification/fcc0c7054f7-09c1-43bf-8f90-c9121bc86e5a/front-end-development-libraries",
+	},
+	{
+		label: "FCC Data Visualization",
+		link: "https://www.freecodecamp.org/certification/fcc0c7054f7-09c1-43bf-8f90-c9121bc86e5a/data-visualization",
+	},
+	{
+		label: "FCC Back End Development and APIs",
+		link: "https://www.freecodecamp.org/certification/fcc0c7054f7-09c1-43bf-8f90-c9121bc86e5a/back-end-development-and-apis",
+	},
+	{
+		label: "Apollo Graph Developer - Associate",
+		link: "https://www.apollographql.com/tutorials/certifications/2877a312-31ad-44a0-bd0c-febb6dc9a4ca",
+	},
+	{
+		label: "Google SkillsShop Diplomas",
+		link: "https://skillshop.exceedlms.com/profiles/e041a50daf52423ca72f738e181e05ed",
+	},
+];
