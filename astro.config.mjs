8955304fe35388b8b6fbfa4df2324d9df25da733
N4N1T0 <68034867+N4N1T0@users.noreply.github.com>
2024-08-04 23:22:24 +0200

    refactor: Performance Improvements

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,33 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-import robotsTxt from 'astro-robots-txt';
+import robotsTxt from "astro-robots-txt";
 import vercel from "@astrojs/vercel/serverless";
 import partytown from "@astrojs/partytown";
-import compress from "astro-compress";
-
+import playformCompress from "@playform/compress";
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: process.env.CI ? 'https://www.adrian-alvarez.dev' : 'http://localhost:4321',
-  integrations: [tailwind(), sitemap(), robotsTxt({
-    sitemap: 'https://www.adrian-alvarez.dev/sitemap-0.xml',
-    host: 'adrian-alvarez.dev'
-  }), partytown({
-    config: {
-      forward: ["dataLayer.push"],
-      debug: false
-    }
-  }), compress(), react()],
-  output: "server",
-  adapter: vercel()
+	site: process.env.CI
+		? "https://www.adrian-alvarez.dev"
+		: "http://localhost:4321",
+	integrations: [
+		tailwind(),
+		sitemap(),
+		robotsTxt({
+			sitemap: "https://www.adrian-alvarez.dev/sitemap-0.xml",
+			host: "adrian-alvarez.dev",
+		}),
+		partytown({
+			config: {
+				forward: ["dataLayer.push"],
+				debug: false,
+			},
+		}),
+		playformCompress(),
+		react(),
+	],
+	output: "server",
+	adapter: vercel(),
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,25 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
 		extend: {
-      spacing: {
-        '8xl': '96rem',
-        '9xl': '128rem',
-      },
-      borderRadius: {
-        '4xl': '2rem',
-      },
+			spacing: {
+				"8xl": "96rem",
+				"9xl": "128rem",
+			},
+			borderRadius: {
+				"4xl": "2rem",
+			},
 			colors: {
-      'background': '#a3bbfb',
-    },
-    fontFamily: {
-      sans: ['Montserrat', 'sans-serif'],
-      prata: ['Prata', 'serif'],
-    },
+				background: "#a3bbfb",
+			},
+			fontFamily: {
+				sans: ["Montserrat", "sans-serif"],
+				prata: ["Prata", "serif"],
+			},
+		},
 	},
-	},
-	plugins: [
-    require('@tailwindcss/typography'),
-  ],
-}
+	plugins: [require("@tailwindcss/typography")],
+};
