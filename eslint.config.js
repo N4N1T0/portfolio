39f091ec76eebedbe9389db51e45c7c498cbbe64
n4n1t0 <adrian.alvarezalonso1991@gmail.com>
2025-02-12 09:32:13 +0100

    feat(package): Update package.json and eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,20 @@
-import eslintPluginAstro from "eslint-plugin-astro";
-import unusedImports from "eslint-plugin-unused-imports";
+import eslintPluginAstro from 'eslint-plugin-astro'
+import unusedImports from 'eslint-plugin-unused-imports'
+import jsxA11y from 'eslint-plugin-jsx-a11y'
 
 export default [
   ...eslintPluginAstro.configs.recommended,
-  ...eslintPluginAstro.configs["jsx-a11y-recommended"],
+  ...eslintPluginAstro.configs['jsx-a11y-recommended'],
   {
-    ignores: [".vscode/*", ".husky/*", "**.lock", "package.json"],
+    ignores: ['.vscode/*', '.husky/*', '**.lock', 'package.json'],
+    file: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx', '**/*.astro'],
     rules: {
-      "@typescript-eslint/no-unused-vars": "off",
-      "unused-imports/no-unused-imports": "error",
+      '@typescript-eslint/no-unused-vars': 'off',
+      'unused-imports/no-unused-imports': 'error'
     },
     plugins: {
-      "unused-imports": unusedImports,
-    },
-  },
-];
+      'unused-imports': unusedImports,
+      'jsx-a11y': jsxA11y
+    }
+  }
+]
