478bc8bd15335c651b730dd6897c9e1db3e39362
n4n1t0 <adrian.alvarezalonso1991@gmail.com>
2025-03-22 16:49:33 +0100

    feat(constants): update social media links and remove outdated diploma services

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,80 +1,3 @@
-export const diplomas = [
-  {
-    label: 'FCC Responsive Web Design',
-    link: 'https://www.freecodecamp.org/certification/fcc0c7054f7-09c1-43bf-8f90-c9121bc86e5a/responsive-web-design'
-  },
-  {
-    label: 'FCC JavaScript Algorithms and Data Structure',
-    link: 'https://www.freecodecamp.org/certification/fcc0c7054f7-09c1-43bf-8f90-c9121bc86e5a/javascript-algorithms-and-data-structures'
-  },
-  {
-    label: 'FCC Front End Development Libraries',
-    link: 'https://www.freecodecamp.org/certification/fcc0c7054f7-09c1-43bf-8f90-c9121bc86e5a/front-end-development-libraries'
-  },
-  {
-    label: 'FCC Data Visualization',
-    link: 'https://www.freecodecamp.org/certification/fcc0c7054f7-09c1-43bf-8f90-c9121bc86e5a/data-visualization'
-  },
-  {
-    label: 'FCC Back End Development and APIs',
-    link: 'https://www.freecodecamp.org/certification/fcc0c7054f7-09c1-43bf-8f90-c9121bc86e5a/back-end-development-and-apis'
-  },
-  {
-    label: 'Apollo Graph Developer - Associate',
-    link: 'https://www.apollographql.com/tutorials/certifications/2877a312-31ad-44a0-bd0c-febb6dc9a4ca'
-  },
-  {
-    label: 'Google SkillsShop Diplomas',
-    link: 'https://skillshop.exceedlms.com/profiles/e041a50daf52423ca72f738e181e05ed'
-  }
-]
-
-export const servicesResponse = [
-  {
-    service: 'desarrollo-en-shopify',
-    response:
-      'Hola, estoy interesado en los servicios de desarrollo en Shopify. ¿Podrían brindarme más información sobre sus paquetes y precios?'
-  },
-  {
-    service: 'desarrollo-en-site-builders',
-    response:
-      'Hola, me gustaría saber más sobre el desarrollo en site builders. ¿Podrían darme detalles sobre cómo trabajan con estas plataformas?'
-  },
-  {
-    service: 'desarrollo-web',
-    response:
-      'Hola, estoy buscando servicios de desarrollo web. ¿Podrían proporcionarme información sobre sus ofertas y tiempos de entrega?'
-  },
-  {
-    service: 'email-marketing',
-    response:
-      'Hola, estoy interesado en sus servicios de email marketing. ¿Podrían enviarme más detalles sobre sus estrategias y costos?'
-  }
-]
-
-export const servicesResponseEnglish = [
-  {
-    service: 'shopify-development',
-    response:
-      'Hello, I am interested in your Shopify development services. Could you provide me with more information about your packages and pricing?'
-  },
-  {
-    service: 'site-builders-development',
-    response:
-      'Hello, I would like to know more about your development services for site builders. Could you give me details about how you work with these platforms?'
-  },
-  {
-    service: 'web-development',
-    response:
-      'Hello, I am looking for web development services. Could you provide me with information about your offerings and delivery times?'
-  },
-  {
-    service: 'email-marketing-en',
-    response:
-      'Hello, I am interested in your email marketing services. Could you send me more details about your strategies and costs?'
-  }
-]
-
 export const shareLinks = [
   {
     label: 'LinkedIn',
@@ -90,5 +13,33 @@ export const shareLinks = [
     label: 'Facebook',
     href: (text: string) =>
       `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(text)}`
+  }
+]
+
+export const socialMedia = [
+  {
+    href: 'https://github.com/N4N1T0',
+    icon: 'https://cdn.simpleicons.org/github/243E61',
+    label: 'GitHub'
+  },
+  {
+    href: 'https://stackoverflow.com/users/23143125/n4n1t0',
+    icon: 'https://cdn.simpleicons.org/stackoverflow/243E61',
+    label: 'Stack Overflow'
+  },
+  {
+    href: 'https://x.com/AdrianlvarezAl1',
+    icon: 'https://cdn.simpleicons.org/x/243E61',
+    label: 'X (Twitter)'
+  },
+  {
+    href: 'https://dev.to/n4n1t0',
+    icon: 'https://cdn.simpleicons.org/devdotto/243E61',
+    label: 'Dev.to'
+  },
+  {
+    href: 'https://www.instagram.com/adrian_alvarez_dev/',
+    icon: 'https://cdn.simpleicons.org/instagram/243E61',
+    label: 'Instagram'
   }
 ]
