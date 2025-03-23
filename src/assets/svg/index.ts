be88127718927c3887dc9b9dcb035c3250b8d9cc
n4n1t0 <adrian.alvarezalonso1991@gmail.com>
2025-03-23 16:57:42 +0100

    feat(assets): add SVG icons for GitHub, Stack Overflow, Instagram, Dev.to, and X

---
+++ src/assets/svg/index.ts
@@ -0,0 +1,7 @@
+import Github from './github.svg'
+import Stackoverflow from './stackoverflow.svg'
+import X from './x.svg'
+import Devto from './devdotto.svg'
+import Instagram from './instagram.svg'
+
+export { Github, Stackoverflow, X, Devto, Instagram }
