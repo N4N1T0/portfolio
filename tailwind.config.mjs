1d62dd7502b0c9ecba231d0a884d3ebdfd69a886
n4n1t0 <adrian.alvarezalonso1991@gmail.com>
2025-03-23 17:04:21 +0100

    feat(tailwind): enable dark mode and update theme colors for improved customization

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,23 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: 'class',
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
       spacing: {
-        "8xl": "96rem",
-        "9xl": "128rem",
+        '8xl': '96rem',
+        '9xl': '128rem'
       },
       borderRadius: {
-        "4xl": "2rem",
+        '4xl': '2rem'
       },
       colors: {
-        background: "#a3bbfb",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)'
       },
       fontFamily: {
-        sans: ["Montserrat", "sans-serif"],
-        prata: ["Prata", "serif"],
-      },
-    },
+        sans: ['Montserrat', 'sans-serif'],
+        prata: ['Prata', 'serif']
+      }
+    }
   },
-  plugins: [require("@tailwindcss/typography")],
-};
+  plugins: [require('@tailwindcss/typography')]
+}
