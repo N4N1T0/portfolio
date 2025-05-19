22a48beaca63d7160d75cfd8470d26f21f1a1df8
n4n1t0 <adrian.alvarezalonso1991@gmail.com>
2025-05-19 12:37:19 +0200

    refactor(animations): replace animejs with tailwind animations for improved performance
    This commit replaces the animejs library with tailwind animations across multiple components to enhance performance and simplify animation management. The changes include updating the animation logic in various components and adjusting the tailwind configuration to support the new animation approach. Additionally, dependencies have been updated to include `@midudev/tailwind-animations` and `@tailwindcss/vite` for better integration with the build system.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,34 +1,26 @@
 import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
 import sitemap from '@astrojs/sitemap'
 import robotsTxt from 'astro-robots-txt'
-import vercel from '@astrojs/vercel'
-import partytown from '@astrojs/partytown'
 import playformCompress from '@playform/compress'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://astro.build/config
 export default defineConfig({
   site: process.env.CI
     ? 'https://www.adrian-alvarez.dev'
     : 'http://localhost:4321',
+
   integrations: [
-    tailwind(),
     sitemap(),
     robotsTxt({
       sitemap: 'https://www.adrian-alvarez.dev/sitemap-0.xml',
       host: 'adrian-alvarez.dev'
     }),
-    partytown({
-      config: {
-        forward: ['dataLayer.push'],
-        debug: false
-      }
-    }),
     playformCompress()
   ],
+
   output: 'static',
-  adapter: vercel(),
-  experimental: {
-    svg: true
+
+  vite: {
+    plugins: [tailwindcss()]
   }
 })
